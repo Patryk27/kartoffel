764c491a09f25450a7c091fb1568255a0ded5774
Patryk Wychowaniec <pwychowaniec@pm.me>
2025-02-17 19:08:44 +0100

    nix: Provide flake.nix
    Closes #7

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+      };
+    };
+
+    utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [
+            (import rust-overlay)
+          ];
+        };
+
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            toolchain
+          ];
+        };
+      }
+    );
+}
